2dd320028b962411bdfd5067c6080874a05e2ea6
buddi-ll <spam.xilef@gmx.de>
2011-02-14 11:33:35 -0800

    deleted github message.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<b>index.php</b> erfolgreich geladen. auf Git-Repository!<br>";
+echo "<b>index.php</b> erfolgreich geladen.<br>";
 require_once ("config.php");
 require_once ("system/sql/sql.php");
 $site=$_GET['site'];
